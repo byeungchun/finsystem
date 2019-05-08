738becb51e51702254f20dbe9278a02fd1405562
Kwon, Byeungchun <byeungchun.kwon@gmail.com>
2019-05-08 17:09:12 +0200

    Create financial institution entity

---
+++ finsystem/entity/fininst.py
@@ -0,0 +1,23 @@
+from dataclasses import dataclass, field
+from abc import ABC, abstractmethod
+from typing import List
+
+from finstmt import BalanceSheet
+
+@dataclass
+class FinancialInstitution(ABC):
+    issuer_name: str
+    country_code: str
+    lei: str = field(init=False, default=None)
+    swift_bic: str = field(init=False, default=None)
+    balance_sheet: List[BalanceSheet] = field(repr=False, init=False)
+    
+    @abstractmethod
+    def generate_balancesheet(self):
+        pass
+
+    
+class Bank(FinancialInstitution):
+    
+    def generate_balancesheet(self):
+        print('generate balance sheet')
